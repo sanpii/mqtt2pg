9e06ba5eadb791bce533a934c30d589568645eef
Sanpi <sanpi@homecomputing.fr>
2016-11-03 13:08:05 +0100

    Use threads

--- src/main.rs
+++ src/main.rs
@@ -13,6 +13,8 @@ use rustc_serialize::json::{self, Json};
 use std::collections::HashMap;
 use std::io::prelude::*;
 use std::fs::File;
+use std::sync::Arc;
+use std::thread;
 
 #[derive(RustcDecodable)]
 pub struct ServerConfig {
@@ -31,68 +33,41 @@ pub struct Config  {
 
 const APP_INFO: AppInfo = AppInfo { name: "mqtt2pg", author: "Sanpi" };
 
-fn main() {
-    let mut connections: HashMap<String, Connection> = HashMap::new();
+fn main()
+{
+    let mut threads = vec![];
 
     let config = match get_config() {
-        Ok(config) => config,
+        Ok(config) => Arc::new(config),
         Err(err) => panic!("Unable to load configuration: {}", err),
     };
 
-    let mut mqtt = match get_mqtt_connection(&config.mqtt) {
-        Ok(mqtt) => mqtt,
-        Err(err) => panic!("Unable to connect to mqtt server: {}", err),
-    };
+    let databases = get_databases(&config.schema);
 
-    mqtt.subscribe("#")
-        .unwrap();
+    for database in databases {
+        let config = config.clone();
 
-    loop {
-        let message = match mqtt.await().unwrap() {
-            Some(message) => message,
-            None => continue,
-        };
+        let thread = thread::spawn(move || {
+            let topic = format!("{}/#", database);
 
-        let topic = message.topic.path.clone();
-        let tokens: Vec<&str> = topic.split('/')
-            .collect();
+            let mut mqtt = match get_mqtt_connection(&config.mqtt) {
+                Ok(mqtt) => mqtt,
+                Err(err) => panic!("Unable to connect to mqtt server: {}", err),
+            };
 
-        let database = tokens[0];
-        let table = tokens[1];
+            let pgsql = match get_pgsql_connection(&config.postgresql, database.as_str()) {
+                Ok(pgsql) => pgsql,
+                Err(e) => panic!("Unable to connect to pgsql server: {:?}", e),
+            };
 
-        let payload = match String::from_utf8((*message.payload).clone()) {
-            Ok(payload) => payload,
-            Err(_) => {
-                println!("payload did not contain valid UTF-8");
-                continue;
-            },
-        };
+            listen(&mut mqtt, pgsql, &config, topic);
+        });
 
-        let json = match Json::from_str(payload.as_str()) {
-            Ok(json) => json,
-            Err(err) => {
-                println!("Invalid json payload: {}", err);
-                continue;
-            },
-        };
+        threads.push(thread);
+    }
 
-        let schema = match config.schema.get(&topic) {
-            Some(schema) => schema,
-            None => {
-                println!("Unknow topic: '{}'.", topic);
-                continue;
-            },
-        };
-
-        let pgsql = match get_pgsql_connection(&mut connections, &config.postgresql, String::from(database)) {
-            Ok(pgsql) => pgsql,
-            Err(e) => {
-                println!("Connection error: {:?}", e);
-                return;
-            },
-        };
-
-        save(&pgsql, table, schema, json);
+    for thread in threads {
+        let _ = thread.join();
     }
 }
 
@@ -138,19 +113,71 @@ fn get_mqtt_connection(config: &ServerConfig) -> Result<Client, String>
     }
 }
 
-fn get_pgsql_connection<'a>(connections: &'a mut HashMap<String, Connection>, config: &ServerConfig, database: String) -> Result<&'a Connection, String>
+fn get_databases(schemas: &HashMap<String, String>) -> Vec<String>
+{
+    let mut databases = Vec::new();
+
+    for topic in schemas.keys() {
+        let tokens: Vec<&str> = topic.split('/').collect();
+        let database = tokens[0].to_string();
+
+        if !databases.contains(&database) {
+            databases.push(database);
+        }
+    }
+
+    databases
+}
+
+fn listen(mqtt: &mut Client, pgsql: Connection, config: &Config, topic: String)
+{
+    mqtt.subscribe(topic.as_str()).unwrap();
+
+    loop {
+        let message = match mqtt.await().unwrap() {
+            Some(message) => message,
+            None => continue,
+        };
+
+        let topic = message.topic.path.clone();
+        let tokens: Vec<&str> = topic.split('/')
+            .collect();
+
+        let table = tokens[1];
+
+        let payload = match String::from_utf8((*message.payload).clone()) {
+            Ok(payload) => payload,
+            Err(_) => {
+                println!("payload did not contain valid UTF-8");
+                continue;
+            },
+        };
+
+        let schema = match config.schema.get(&topic) {
+            Some(schema) => schema,
+            None => panic!("Unknow topic: '{}'.", topic),
+        };
+
+        let json = match Json::from_str(payload.as_str()) {
+            Ok(json) => json,
+            Err(err) => {
+                println!("Invalid json payload: {}", err);
+                continue;
+            },
+        };
+
+        save(&pgsql, table, schema, json);
+    }
+}
+
+fn get_pgsql_connection(config: &ServerConfig, database: &str) -> Result<Connection, String>
 {
     let dsn = format!("postgres://{}:{}@{}:{}/{}", config.username, config.password, config.server, config.port, database);
 
-    if !connections.contains_key(&database) {
-        let connection = match Connection::connect(dsn.as_str(), TlsMode::None) {
-            Ok(connection) => connection,
-            Err(err) => return Err(format!("{:?}", err)),
-        };
-        connections.insert(database.clone(), connection);
+    match Connection::connect(dsn.as_str(), TlsMode::None) {
+        Ok(connection) => Ok(connection),
+        Err(err) => return Err(format!("{:?}", err)),
     }
-
-    Ok(connections.get(&database).unwrap())
 }
 
 fn save<T, U>(connection: &Connection, table: T, schema: U, json: Json) where T: std::fmt::Display, U: std::fmt::Display
